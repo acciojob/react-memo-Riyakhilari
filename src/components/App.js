8cc16d70d1ce98d8714876760d623fecd5733b21
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-02-26 22:26:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,33 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import TodoList from "./components/TodoList";
+import UseMemo from "./components/UseMemo";
+import ReactMemo from "./components/ReactMemo";
 
+const App = () => {
+  const [todos, setTodos] = useState(["New todo"]);
+  const [count, setCount] = useState(0);
+
+  const addTodo = () => {
+    setTodos([...todos, "New todo"]);
+  };
+
+  return (
+    <div>
+      <h1>React.useMemo</h1>
+      <h2>My todos</h2>
+      <button onClick={addTodo}>Add Todo</button>
+      <p>Count: {count} <button onClick={() => setCount(count + 1)}>+</button></p>
+
+      <h2>Expensive Calculation</h2>
+      <UseMemo count={count} />
+
+      <h2>React.memo</h2>
+      <ReactMemo />
+      
+      <h3>Todo List</h3>
+      <TodoList todos={todos} />
+    </div>
+  );
+};
+
+export default App;
