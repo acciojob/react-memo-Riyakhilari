99c6002cc089525a7a3ed5af381684419672b346
Riya <149504570+Riyakhilari@users.noreply.github.com>
2025-03-03 22:09:36 +0530

    Update UseMemo.js

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -1,24 +1,23 @@
-
 import React, { useMemo, useState } from "react";
 
 const UseMemo = () => {
   const [count, setCount] = useState(0);
 
-  const result = useMemo(() => {
-    return count * 2;
-  }, [count]);
+  const result = useMemo(() => count * 2, [count]);
 
   const onIncrease = () => {
-    setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
   };
+
   return (
     <div>
+      <h2>Counter</h2>
       <div>
-        <span>Count:{count}</span>
-        <button onClick={onIncrease}>+</button>
+        <span>Count: {count}</span>
+        <button id="increment-btn" onClick={onIncrease}>+</button>
       </div>
       <h2>Expensive Calculation</h2>
-      {result}
+      <p>{result}</p>
     </div>
   );
 };
